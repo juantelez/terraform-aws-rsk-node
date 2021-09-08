8c8853d25817b60828fc957256e0137eda929cb4
Juan Telez <juan@iovlabs.org>
2021-09-08 15:23:12 -0300

    Description added for public ip output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "public_ip" {
-value = module.ec2_instance.public_ip
+  description = "The public IP for the EC2 instance where your RSK node run"
+  value       = module.ec2_instance.public_ip
 }
